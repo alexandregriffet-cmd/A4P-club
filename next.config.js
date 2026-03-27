46d4d082c23ae3f964340643657070d2ba674ad6
alexandregriffet-cmd <alexandre.griffet@gmail.com>
2026-03-27 19:19:01 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import(‘next’).NextConfig} */
+const nextConfig = {
+experimental: {
+appDir: true,
+},
+}
+
 module.exports = nextConfig
